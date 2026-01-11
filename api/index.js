11f85270e909d3d1d965573a7b69ea8d96e7c038
unknown <salunkheshreya19@gmail.com>
2026-01-12 02:29:12 +0530

    chngaes amde

--- api/index.js
+++ api/index.js
@@ -1,4 +1,6 @@
 import serverless from "serverless-http";
 import { app } from "../src/app.js";
 
+app.get("/", (req, res) => res.send("API is working!"));
+
 export const handler = serverless(app);

66d9865a52e072a3fa89733338f490e0ddcded5f
unknown <salunkheshreya19@gmail.com>
2026-01-18 01:22:00 +0530

    change

--- api/index.js
+++ api/index.js
@@ -1,5 +1,5 @@
 import serverless from "serverless-http";
-import { app } from "../src/app.js";
+import { app } from "../src/app/app.js";
 
 app.get("/", (req, res) => res.send("API is working!"));
 

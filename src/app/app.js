75a8d8a923ddd41415dd9b95aeaefef311dcd4ad
unknown <salunkheshreya19@gmail.com>
2026-01-18 02:10:03 +0530

    change

--- src/app/app.js
+++ src/app/app.js
@@ -1,27 +1,25 @@
-import cookieParser from "cookie-parser";
 import cors from "cors";
 import "dotenv/config";
 import express from "express";
 
-import router from "../routes/router.js";
 
 export const app = express();
 // export const server = http.createServer(app);
 
 // base middleware
 app.use(cors());
-app.use(cookieParser());
+// app.use(cookieParser());
 app.use(express.json({ limit: "2gb" }));
 app.use(express.urlencoded({ limit: "2gb", extended: true }));
 
-app.get("/health", (req, res) => {
-  res.send("OK");
-});
+// app.get("/health", (req, res) => {
+//   res.send("OK");
+// });
 
 
 // server.listen(config.PORT, () =>
 //   console.log("listening on port " + config.PORT)
 // );
 
-app.use(router);
+// app.use(router);
 //
--- src/server.js
+++ src/server.js
@@ -1,13 +1,12 @@
 import { app } from "./app/app.js";
-import config from "./config/config.js";
 import connectDatabase from "./config/dbConnection.js";
 import { errorHandler } from "./middleware/errorHandler.js";
 import router from "./routes/router.js";
 
+connectDatabase();
 app.use("/", router);
 // app.use(notFound);
 app.use(errorHandler);
-connectDatabase();
-app.listen(config.PORT, () => {
-  console.log("Listening on port " + config.PORT);
-});
+// app.listen(config.PORT, () => {
+//   console.log("Listening on port " + config.PORT);
+// });

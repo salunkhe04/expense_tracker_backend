2c9a21308b580add1cdc22c2f2a12679fcdd4814
unknown <salunkheshreya19@gmail.com>
2026-01-18 02:47:24 +0530

    change

--- src/server.js
+++ src/server.js
@@ -1,5 +1,4 @@
 import { app } from "./app/app.js";
-import config from "./config/config.js";
 import connectDatabase from "./config/dbConnection.js";
 import { errorHandler } from "./middleware/errorHandler.js";
 import router from "./routes/router.js";
@@ -8,6 +7,8 @@ connectDatabase();
 app.use("/", router);
 // app.use(notFound);
 app.use(errorHandler);
-app.listen(config.PORT, () => {
-  console.log("Listening on port " + config.PORT);
-});
+// app.listen(config.PORT, () => {
+//   console.log("Listening on port " + config.PORT);
+// });
+
+export default app;
